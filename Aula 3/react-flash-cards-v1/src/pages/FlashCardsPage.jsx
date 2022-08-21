0e0f19d681a28ffc7f04a38ab806388fb95b53c4
Douglas Peron <peron930@gmail.com>
2022-08-21 00:03:19 -0300

    Aula 3 Flashcards-v1
    Criação de button para randomizar cards apresentados, e useState para setar valor do shuffler

--- Aula 3/react-flash-cards-v1/src/pages/FlashCardsPage.jsx
+++ Aula 3/react-flash-cards-v1/src/pages/FlashCardsPage.jsx
@@ -1,18 +1,32 @@
+import { useState } from "react";
+import Button from "../components/Button";
 import Flashcard from "../components/Flashcard";
 import Flashcards from "../components/Flashcards";
 import Header from "../components/Header";
 import Main from "../components/Main";
 import { AllFlashcards } from "../data/AllFlashcards";
-console.log(AllFlashcards);
+import { helperShuffleArray } from "../helpers/arrayHelpers";
 
 function FlashCardsPage() {
+  const [allCards, setAllCards] = useState(AllFlashcards);
+
+  function handleButtonClick() {
+    const shuflleCards = helperShuffleArray(allCards);
+    setAllCards(shuflleCards);
+    console.log(setAllCards);
+  }
   return (
     <>
       <Header>React Flash-cards</Header>
 
       <Main>
+        <div className="mb-4 text-center">
+          <Button onButtonClick={handleButtonClick}>
+            Embaralhar Flashcards
+          </Button>
+        </div>
         <Flashcards>
-          {AllFlashcards.map((flashcard) => {
+          {allCards.map((flashcard) => {
             return (
               <Flashcard
                 key={flashcard.id}
