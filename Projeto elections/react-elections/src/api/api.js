ec7d3a45cac2e36871a7326fb9c91ccc5cc88962
Douglas Peron <peron930@gmail.com>
2022-08-23 22:54:03 -0300

    render elections
    Criação e finalização de renderização dos candidatos votos e cidades com add de CACHE para limitar requisições ao BACK

--- Projeto elections/react-elections/src/api/api.js
+++ Projeto elections/react-elections/src/api/api.js
@@ -1,30 +1,50 @@
-import AxiosModule from 'axios'
+import AxiosModule from "axios";
 
-const axios = AxiosModule.create({baseURL: 'http://localhost:3001'})
-async function getAllCities(){
-    const {date: cities} = await axios.axios.get('/cities')
-    const sortedCities = cities.sort((a,b) => a.name.localeCompare(b.name))
-    return sortedCities
+const axios = AxiosModule.create({ baseURL: "http://localhost:3001" });
+
+const CACHE = {};
+
+async function getAllCities() {
+  if (CACHE.allCities) {
+    return CACHE.allCities;
+  }
+  const { date: cities } = await axios.axios.get("/cities");
+  const sortedCities = cities.sort((a, b) => a.name.localeCompare(b.name));
+  CACHE.allCities = sortedCities;
+  return sortedCities;
 }
 
-async function getAllCandidates(){
-    const {date: candidates} = await axios.axios.get('/candidates')
-    return candidates
+async function getAllCandidates() {
+  if (CACHE.allCandidates) {
+    return CACHE.allCandidates;
+  }
+  const { date: candidates } = await axios.axios.get("/candidates");
+  CACHE.allCandidates = candidates;
+  return candidates;
 }
 
-async function getElection(cityId){
-    const getAllCities = await getAllCities()
-    const city = getAllCities.find(city => city.id === cityId)
-    const getAllCandidates = await getAllCandidates()
+async function getElection(cityId) {
+  if (CACHE[cityId]) {
+    return CACHE[cityId];
+  }
+  const getAllCities = await getAllCities();
+  const city = getAllCities.find((city) => city.id === cityId);
+  const getAllCandidates = await getAllCandidates();
 
-    const {date: electionsData} = await axios.axios.get(`/election?cityId=${cityId}`)
-    const finalData = {elections: electionsData.map(item => {
-        const {name, username} = getAllCandidates.find(
-            candidate => candidate.id === item.candidateId)
-            return {...item, name, username}
-    }), city}
-    return electionsData
-
+  const { date: electionsData } = await axios.axios.get(
+    `/election?cityId=${cityId}`
+  );
+  const finalData = {
+    elections: electionsData.map((item) => {
+      const { name, username } = getAllCandidates.find(
+        (candidate) => candidate.id === item.candidateId
+      );
+      return { ...item, name, username };
+    }),
+    city,
+  };
+  CACHE.allCandidates = finalData;
+  return finalData;
 }
-export{getAllCities, getAllCandidates, getElection}
+export { getAllCities, getAllCandidates, getElection };
 //api para pegar do back os dados
--- Projeto elections/react-elections/src/pages/ElectionsPage.jsx
+++ Projeto elections/react-elections/src/pages/ElectionsPage.jsx
@@ -1,42 +1,120 @@
-
-
 console.log(allInformation);
 
 function ElectionsPage() {
-  const [loadingPage, setLoadingPage] = useState(true)
-  const [allCities, setAllCities] = useState([])
-  const [selectedCity, setSelectedCity] = useState(null)
-  const [loadingElections, setLoadingElections] = useState(true)
-  const [currentElections, setCurrentElections] = useState([])
+  const [loadingPage, setLoadingPage] = useState(true);
+  const [allCities, setAllCities] = useState([]);
+  const [selectedCity, setSelectedCity] = useState(null);
+  const [loadingElections, setLoadingElections] = useState(true);
+  const [currentElections, setCurrentElections] = useState([]);
 
   useEffect(() => {
-    setLoadingPage(true)
-    async function getBackendCities(){
-      const backendCities = await ApiModule.getBackendCities()
-      setAllCities(backendCities)
-      setLoadingPage(false)
+    if (!selectedCity) {
+      return;
     }
-  })
-  function handleButtonClick({currentTarget}){
-    setSelectedCity(currentTarget.value)
-    console.log(currentTarget)
+    async function getBackendCities() {
+      setLoadingPage(true);
+      const backendCities = await ApiModule.getAllCities();
+      setAllCities(backendCities);
+      setSelectedCity(backendCities[0].id);
+      setLoadingPage(false);
+    }
+    getBackendCities();
+  }, []);
+
+  useEffect(() => {
+    async function getBackendElections() {
+      setLoadingElections(true);
+      const backendCities = await ApiModule.getElection(selectedCity);
+      setCurrentElections(backendCities);
+      setLoadingElections(false);
+    }
+    getBackendElections();
+  }, [selectedCity]);
+
+  async function handleButtonClick({ currentTarget }) {
+    const newSelectedCity = currentElections.value;
+    setSelectedCity(currentTarget.value);
+    console.log(currentTarget);
   }
-  if(loadingPage){
-    return(
-      <div><ClipLoader/></div>
-    )
+  if (loadingPage) {
+    return (
+      <div Classname="text-center mt-4">
+        <ClipLoader />
+      </div>
+    );
   }
-   return (
+
+  const buttonsJsx = (
+    <ul Classname="flex flex-row flex-wrap items-center justfy-start">
+      {allCities.map(({ id, name }) => {
+        const selectedClassName = id === selectedCity ? "bg-gray-800" : "";
+        return (
+          <li key={id}>
+            <button
+              onClick={handleButtonClick}
+              value={id}
+              Classname={`bg-gray=200 m-2 p-2 hover:bg-gray-400 hover: bg-gray-600 ${selectedClassName}`}
+            >
+              {name}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+
+  let electionsJsx = (
+    <div Classname="text-center mt-4">
+      <ClipLoader />
+    </div>
+  );
+  if (!loadingElections) {
+    const { city, elections } = currentElections;
+    const { name, votingPopulation, absence, presence } = city;
+
+    electionsJsx = (
+      <div>
+        <h2 className="text-center mt-4 text-2xl font-semibold">
+          Dados de {name}
+        </h2>
+
+        <p>Total de eleitores: {votingPopulation}</p>
+        <p>Abstenções: {absence}</p>
+        <p>votos Válidos: {presence}</p>
+
+        <ul className="flex flex-row flex-wrap items-center justfy-start ">
+          {elections.map((item) => {
+            const { name: candidateName, username, votes } = item;
+            const isElected = index === 0;
+            return (
+              <li
+                className="m-2 w-48 h-48 shadow-lg flex flex-col items-center justify-between px-2 hover: bg-gray-100"
+                key={id}
+              >
+                <div className="flex flex-row items-center justify-between w-full p-4">
+                  <img
+                    className="rounded-full w-12"
+                    src={`/img/${username}.png`}
+                    alt={candidateName}
+                  />
+                  <div>
+                    <span>{votes}</span>
+                    <span>{((votes / presence) * 100).toFixed(2)}%</span>
+                  </div>
+                </div>
+                <div>{candidateName}</div>
+                <div>{isElected ? "Eleito" : "Não eleito"}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+  return (
     <>
-      <Header>React Elections</Header>
-      <ul Classname="flex flex-row flex-wrap items-center justfy-start">
-        
-        {allCities.map(({id, name}) => {
-          const selectedClassName = id === selectedCity ? 'bg-gray-800': ''
-          return <li key={id}><button onClick={handleButtonClick} value={id} Classname={`bg-gray=200 m-2 p-2 hover:bg-gray-400 ${selectedClassName}`}> {name}</button></li>
-        })}
-      </ul>
-      <Main></Main>
+      {buttonsJsx}
+      {electionsJsx}
     </>
   );
 }
